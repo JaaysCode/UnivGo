01b829e772ba714e4f59aeadc0a8e05caa987378
Pablo <jpcardonab0520@gmail.com>
2025-06-10 17:12:56 -0500

    Update: Format code

--- apps/web/components/Button.tsx
+++ apps/web/components/Button.tsx
@@ -1,6 +1,27 @@
 
 export const Button = ({text}: {text:string}) => {
     return(
-        <button  className="text-white bg-[#F12229] hover:bg-[#D32229] font-medium rounded-xl text-md px-16 py-2.5 text-center me-2 mb-2 transition-transform duration-200 ease-in-out hover:scale-105">{text}</button>
+        <button 
+            className="
+                text-white 
+                bg-[#F12229]
+                hover:bg-[#D32229]
+                font-medium 
+                rounded-xl 
+                text-md 
+                px-16 
+                py-2.5 
+                text-center 
+                me-2 
+                mb-2 
+                transition-transform 
+                duration-200 
+                ease-in-out 
+                hover:scale-105
+                cursor-pointer
+            ">
+                    {text}
+        </button>
     )
+
 }
