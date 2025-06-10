580f7b70e861ce50e4f1740595bc42d42880ea8e
SamAl404 <samualvarezh@gmail.com>
2025-06-09 20:10:28 -0500

    feat: provitional view of the login

--- apps/web/components/auth/Login.tsx
+++ apps/web/components/auth/Login.tsx
@@ -1,11 +1,30 @@
 import InputForm from "../InputForm" 
 import {Button} from "../Button"
 import Ulogo from "../Ulogo"
+import Image from "next/image"
 
 export function Login(){
     return(
         <>
-            <Ulogo></Ulogo>
+        <div className="flex min-h-screen"> {/* Main flex container */}
+            {/* Left side - Image */}
+            <div className="w-1/2 bg-[url(/imagen_u_medellin.jpg)] bg-cover bg-center bg-no-repeat"> 
+                
+            </div>
+
+            {/* Right side - Login Form */}
+            <div className="w-1/2 flex flex-col items-center justify-center p-8 "> {/* Adjust width and padding as needed */}
+                <Ulogo />
+                <div className="w-full max-w-xs"> {/* Optional: constrain width of form elements */}
+                    <InputForm placeholder="Documento identidad" typeInput = "text"/>
+                    <InputForm placeholder="Contraseña" typeInput = "password"/>
+                    <div className="flex justify-center mt-6">
+                        <Button text="Entrar"/>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
         </>
     )
 }
