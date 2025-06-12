1e058862f0ccff755645a8977080f614c178947e
Pablo <jpcardonab0520@gmail.com>
2025-06-11 21:04:59 -0500

    Feat: Footer component completed

---
+++ apps/web/components/footer/Footer.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import SocialMediaIcons from './SocialMediaIcons'
+import FooterInfo from './FooterInfo'
+import Ulogo from '../Logo'
+const Footer = () => {
+  return (
+    <footer className='bg-[var(--primary-red)] text-white '>      
+        <div className='max-w-[95%] mx-auto px-[50px] py-[25px] flex items-center justify-between border-b border-white'>
+            <SocialMediaIcons/>
+        </div>
+        <div className='max-w-[95%] mx-auto px-[50px] py-[25px] border-b border-white'>
+            <FooterInfo/>
+        </div>    
+        <div className='flex items-center justify-center w-full py-8'>
+            UnivGo © 2025. Proyecto desarrollado para la Universidad de Medellín.
+        </div>
+        
+    </footer>
+  )
+}
+
+export default Footer
