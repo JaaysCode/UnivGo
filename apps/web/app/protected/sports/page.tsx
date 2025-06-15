72636ad50b384dd204ac31d620dc61ba6274e072
Pablo <jpcardonab0520@gmail.com>
2025-06-15 15:13:37 -0500

    feat: add sports page

---
+++ apps/web/app/protected/sports/page.tsx
@@ -0,0 +1,14 @@
+import { Navbar } from "@/components/navbar/Navbar"
+import Footer from "@/components/footer/Footer";
+
+import React from 'react'
+
+export default function SportsPage() {
+  return (
+    <>
+        <Navbar/>
+        <div></div>
+        <Footer/>
+    </>
+  )
+}
