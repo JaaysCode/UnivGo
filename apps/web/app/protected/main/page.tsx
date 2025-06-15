8d4d1c3e2a85f0aca50ec2d9526ae02f355d9615
Pablo <jpcardonab0520@gmail.com>
2025-06-15 15:15:12 -0500

    Feat: deleted vercel & images, mini refactor of ChoiceModal and OptionCard

--- apps/web/app/protected/main/page.tsx
+++ apps/web/app/protected/main/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 import Footer from "@/components/footer/Footer";
 import { Navbar } from "@/components/navbar/Navbar";
-import OptionCard from "@/components/main/OptionCard";
-import ChoiceModal from "@/components/main/ChoiceModal";
+import OptionCard from "../../../components/OptionCard";
+import ChoiceModal from "@/components/modals/choice-modal/ChoiceModal";
 import React, { useState } from "react";
+import { FaBuilding } from "react-icons/fa";
+import { IoIosBasketball } from "react-icons/io";
+
 
 export default function Main() {
   const [showChoiceModal, setShowChoiceModal] = useState(false);
@@ -12,26 +15,35 @@ export default function Main() {
     <>
       <Navbar />
       <OptionCard
-      title="Reservar espacios"
-      description="¿Necesitas un lugar para tu próximo partido, evento o reunión? Reserva nuestros espacios deportivos y áreas comunes con solo un clic. Disfruta de canchas de fútbol, baloncesto, salones multifuncionales y zonas al aire libre, equipadas con iluminación óptima y acceso seguro. Elige tu horario preferido, invita a tus compañeros y garantiza tu espacio sin complicaciones. ¡Gestionamos todo para que tú solo te preocupes por divertirte!"
-      imageSrc="/imagen_coliseo.jpg"
-      buttonPosition="right"
-      buttonText="Reservar"
-      buttonOnClick={() => setShowChoiceModal(true)}
+        title="Reservar espacios"
+        description="¿Necesitas un lugar para tu próximo partido, evento o reunión? Reserva nuestros espacios deportivos y áreas comunes con solo un clic. Disfruta de canchas de fútbol, baloncesto, salones multifuncionales y zonas al aire libre, equipadas con iluminación óptima y acceso seguro. Elige tu horario preferido, invita a tus compañeros y garantiza tu espacio sin complicaciones. ¡Gestionamos todo para que tú solo te preocupes por divertirte!"
+        imageSrc="/imagen_coliseo.jpg"
+        buttonPosition="right"
+        buttonText="Reservar"
+        buttonOnClick={() => setShowChoiceModal(true)}
       />
       <OptionCard
-      title="Mi horario"
-      description="Consulta toda tu información académica en un solo lugar. Revisa tu horario de clases, las materias inscritas, los grupos asignados y tu progreso con notas parciales y promedio acumulado. Todo organizado para que tengas claridad sobre tu semestre."
-      imageSrc="/udemedellin_estudiantes.jpg"
-      buttonPosition="left"
-      buttonText="Consultar"
+        title="Mi horario"
+        description="Consulta toda tu información académica en un solo lugar. Revisa tu horario de clases, las materias inscritas, los grupos asignados y tu progreso con notas parciales y promedio acumulado. Todo organizado para que tengas claridad sobre tu semestre."
+        imageSrc="/udemedellin_estudiantes.jpg"
+        buttonPosition="left"
+        buttonText="Consultar"
       />
       <Footer />
+
       <ChoiceModal
         open={showChoiceModal}
         onClose={() => setShowChoiceModal(false)}
-        option1={{ label: "Espacio deportivo", onClick: () => {/* acción para opción 1 */}, icon: <i></i> }}
-        option2={{ label: "Área común", onClick: () => {/* acción para opción 2 */}, icon: <i></i> }}
+        option1={{
+          label: "Espacio deportivo",
+          route: "/protected/sports",
+          icon: IoIosBasketball
+        }}
+        option2={{
+          label: "Área común",
+          route: "/protected/common",
+          icon: FaBuilding
+        }}
       />
     </>
 
