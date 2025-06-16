6b1d7d5f0900072b477f62f0c87c0af3a59a3020
SamAl404 <samualvarezh@gmail.com>
2025-06-15 21:19:47 -0500

    feat: add option cards to the sports reservations page

--- apps/web/app/protected/sports/page.tsx
+++ apps/web/app/protected/sports/page.tsx
@@ -1,17 +1,60 @@
-import { Navbar } from "@/components/navbar/Navbar"
+import { Navbar } from "@/components/navbar/Navbar";
 import Footer from "@/components/footer/Footer";
+import OptionCard from "@/components/OptionCard";
 
-import React from 'react'
+import React from "react";
 
 export default function SportsPage() {
   return (
     <>
-        <Navbar/>
-        <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-4xl font-bold mb-4">Sports Page</h1>
-            <p className="text-lg">This is the sports page content.</p>
+      <Navbar />
+      <div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4">
+          <OptionCard
+            title="Gimnasio"
+            description="¡Actívate en nuestro gimnasio universitario! Tu lugar para desconectarte, recargar y poner tu cuerpo en movimiento. ¡Te esperamos para que te sientas genial!"
+            buttonhref="/sports/gym"
+            buttonText="Reservar"
+            buttonPosition="left"
+            imageSrc="/udemgym.jpg"
+          />
+          <OptionCard
+            title="Coliseo"
+            description="Tu epicentro para arte, música, teatro y eventos inolvidables. ¡Ven a vivir la cultura universitaria!"
+            buttonhref="/sports/coliseum"
+            buttonText="Reservar"
+            buttonPosition="right"
+            imageSrc="/Coliseo_UdeM.jpg"
+          />
+          <OptionCard
+            title="Cancha de grama sintética fútbol 11"
+            description="¡Siente la pasión del fútbol en nuestra cancha de grama sintética! Tu lugar para goles, jugadas épicas y la emoción del fútbol 11. ¡A rodar el balón!"
+            buttonhref="/sports/football11"
+            buttonText="Reservar"
+            buttonPosition="left"
+            imageSrc="/cancha-fut11.jpg"
+          />
+          <OptionCard
+            title="Cancha de grama sintética fútbol 7"
+            description="El lugar perfecto para partidos rápidos, goles espectaculares y momentos inolvidables con tus amigos. ¡La cancha te espera!"
+            buttonhref="/sports/football7"
+            buttonText="Reservar"
+            buttonPosition="right"
+            imageSrc="/cancha-fut7.jpg"
+          />
+          <div className="">
+            <OptionCard
+            title="Canchas de tenis"
+            description="Perfecciona tu saque, domina tu revés y vive la emoción de cada punto. ¡Es hora de jugar!"
+            buttonhref="/sports/tenis"
+            buttonText="Reservar"
+            buttonPosition="left"
+            imageSrc="/campo-tenis-generico.jpg"
+            />
+          </div>
         </div>
-        <Footer/>
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
